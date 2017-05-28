6183013f62fd442799d85804191093b8a4aec67e
hareku <hareku908@gmail.com>
2017-05-28 23:41:52 +0900

    update for php7.0

--- src/FollowServiceProvider.php
+++ src/FollowServiceProvider.php
@@ -13,13 +13,11 @@ class FollowServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->publishes([
-            __DIR__.'/../config/follow.php' => config_path('follow.php'),
-        ], 'config');
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/follow.php', 'follow'
+        );
 
-        $this->publishes([
-            __DIR__.'/../database/migrations/0000_00_00_000000_create_follow_relationships_table.php' => database_path('migrations/'.date('Y_m_d_His').'_create_follow_relationships_table.php'),
-        ], 'migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations/2017_05_20_000000_create_follow_relationships_table.php');
     }
 
     /**
